5c201b789d46f925f94ebd44470e5a16bfd46ea6
Jose Garcia <josegarcia19809@gmail.com>
2023-11-19 08:19:58 -0600

    Inicio de proyecto

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {registrar, perfil, confirmar, autenticar} from "../controllers/veterinarioControllers.js"
+
+const router = express.Router();
+
+router.post("/", registrar);
+router.get("/perfil", perfil);
+router.get("/confirmar/:token", confirmar);
+router.post("/login", autenticar)
+
+export default router;
