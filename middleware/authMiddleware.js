f0ec35190fb61affefa672283ca3c134959ab2c1
Jose Garcia <josegarcia19809@gmail.com>
2023-11-20 10:22:30 -0600

    Comprobando el token en el middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,10 @@
-const checkAuth=(req, res, next)=>{
-    console.log("desde middleware");
+const checkAuth = (req, res, next) => {
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        console.log("Si tiene el token con bearer");
+    }
+
+    const error = new Error("Token no válido o inexistente");
+    res.status(403).json({msg: error.message})
 
     next();
 }
