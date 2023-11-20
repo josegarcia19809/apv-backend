12f05199071eaeae587800c7a1fd91e6dde3dff9
Jose Garcia <josegarcia19809@gmail.com>
2023-11-20 12:00:24 -0600

    Almacenar el Nuevo Password

--- controllers/veterinarioControllers.js
+++ controllers/veterinarioControllers.js
@@ -99,8 +99,23 @@ const comprobarToken = async (req, res) => {
         return res.status(400).json({msg: error.message});
     }
 };
-const nuevoPassword = (req, res) => {
+const nuevoPassword = async (req, res) => {
+    const {token} = req.params;
+    const {password} = req.body;
 
+    const veterinario = await Veterinario.findOne({token});
+    if (!veterinario) {
+        const error = new Error("Hubo un error");
+        return res.status(400).json({msg: error.message});
+    }
+    try {
+        veterinario.token = null;
+        veterinario.password = password;
+        await veterinario.save();
+        res.json({msg: "Password modificado correctamente"});
+    } catch (e) {
+        console.log(e)
+    }
 };
 
 export {
